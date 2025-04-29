501a1ed2e9fe280bce95227b7aefc8de9d3a542b
cicrilhos <gabrielmaletzkedeavelar@gmail.com>
2025-04-29 15:04:56 -0300

    envio

--- home/script.js
+++ home/script.js
@@ -1,10 +1,25 @@
 function alternarMenu() {
-    const menu = document.getElementById('menu-lateral');
+    const menu = document.getElementById('menu');
     const estiloAtual = window.getComputedStyle(menu).display;
 
     if (estiloAtual === 'none') {
-        menu.style.display = 'block';
+        menu.style.display = 'flex'; // Exibe o menu
+        menu.style.height = 'auto'; // Ajusta a altura para ocupar espaço
     } else {
-        menu.style.display = 'none';
+        menu.style.display = 'none'; // Esconde o menu
+        // Remover a linha abaixo, pois não é necessária
+        // menu.style.height = '0'; // Remove o espaço ocupado
     }
 }
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const modal = document.getElementById('welcomeModal');
+    const startButton = document.getElementById('startButton');
+    const audio = document.getElementById('audioPlayer');
+
+    startButton.addEventListener('click', function () {
+        modal.style.display = 'none';
+        audio.play();
+    });
+});
